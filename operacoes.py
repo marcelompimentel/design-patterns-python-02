12330914147f11fd291aadf5246d244e1e4a253b
marcelompimentel <marcelompimentel@gmail.com>
2020-05-04 21:48:33 -0300

    Aplicação do padrão de projeto Interpreter

---
+++ operacoes.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+
+class Soma(object):
+    def __init__(self, expressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = expressao_esquerda
+        self.__expressao_direita = expressao_direita
+
+    def avalia(self):
+        return self.__expressao_esquerda.avalia() + self.__expressao_direita.avalia()
+
+
+class Subtracao(object):
+    def __init__(self, expressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = expressao_esquerda
+        self.__expressao_direita = expressao_direita
+
+    def avalia(self):
+        return self.__expressao_esquerda.avalia() - self.__expressao_direita.avalia()
+
+
+class Numero(object):
+    def __init__(self, numero):
+        self.__numero = numero
+
+    def avalia(self):
+        return self.__numero
+
+
+if __name__ == '__main__':
+    expressao_esquerda = Soma(Numero(25), Numero(35))
+    expressao_direita = Subtracao(Numero(10), Numero(5))
+    expressao_conta = Soma(expressao_esquerda, expressao_direita)
+    print(expressao_conta.avalia())
